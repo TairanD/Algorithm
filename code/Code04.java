7209b17f23a780abed8e054dd5f99398fc5605a9
Tairan Ding <tairan.ding@ucdconnect.ie>
2024-04-11 20:57:12 +0800

    update linked list code

--- code/Code04.java
+++ code/Code04.java
@@ -1,17 +1,21 @@
 package code;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Stack;
+
 public class Code04 {
-    public static class Node<V>{
+    public static class Node<V extends Comparable<V>>{
         V value;
         public Node<V> next;
 
         public Node(V v){this.value = v;};
     }
 
-    public static <V> Node<V> reverseList(Node<V> head){
+    public static <V extends Comparable<V>> Node<V> reverseList(Node<V> head){
         // initialize current head node of the list
         Node<V> currentHead = head;
-        // for each node after head, i'd like to swap it with current head node of the slinked list
+        // for each node after head, I'd like to swap it with current head node of the slinked list
         while (head.next!=null){
             // swap originalHead.next to the currentHead
             Node<V> next = head.next;
@@ -22,7 +26,7 @@ public class Code04 {
         return currentHead;
     }
 
-    public static <V> Node<V> reverseList2(Node<V> head){
+    public static <V extends Comparable<V>> Node<V> reverseList2(Node<V> head){
         Node<V> currentHead = head;
         Node<V> next = head.next;
         while (next!=null){
@@ -34,7 +38,7 @@ public class Code04 {
         return currentHead;
     }
 
-    public static <V> void printLinkedList(Node<V> head) {
+    public static <V extends Comparable<V>> void printLinkedList(Node<V> head) {
         System.out.print("Linked List: ");
         while (head != null) {
             System.out.print(head.value + " ");
@@ -69,9 +73,10 @@ public class Code04 {
         return currentHead;
     }
 
-    public static void printDoubleLinkedList(DoubleNode head) {
+
+    public static <V> void printDoubleLinkedList(DoubleNode<V> head) {
         System.out.print("Double Linked List: ");
-        DoubleNode end = null;
+        DoubleNode<V> end = null;
         while (head != null) {
             System.out.print(head.value + " ");
             end = head;
@@ -84,6 +89,95 @@ public class Code04 {
         }
         System.out.println();
     }
+
+    public static <V extends Comparable<V>> void printSharedPart(Node<V> list1, Node<V> list2){
+        // list1 & list2 respectively represents the head node of list1 & list2, which are both ordered list
+        ArrayList<Node<V>> share = new ArrayList<>();
+        Node<V> p1 = list1;
+        Node<V> p2 = list2;
+        while (p1!=null && p2!=null){
+            int result = p1.value.compareTo(p2.value);
+            if (result == 0){
+                share.add(p1);
+                p1 = p1.next;
+                p2 = p2.next;
+            }else if (result<0){
+                p1 = p1.next;
+            }else {
+                p2 = p2.next;
+            }
+        }
+        for (Node<V> node: share){
+            System.out.println("shared value: " + node.value);
+        }
+    }
+
+    public static <V extends Comparable<V>> boolean ifPalindrome(Node<V> head){
+        if (head == null){
+            return false;
+        }
+        Stack<V> stack = new Stack<>();
+        Node<V> slowerPointer = head;
+        Node<V> fasterPointer = head;
+
+        // I want the slower pointer be the middle node of the list
+        // and when the length of the list is even
+        while (true){
+            // if slowerPointer = null, meaning that the list has only a head.
+            if (slowerPointer.next==null){
+                return true;
+            }
+
+            // (fasterPointer.next == null) means that the length of the list is odd
+            // (because we start at head and jump two pace once a time for faster pointer)
+            // and at this time, slowerPointer is at the middle location!
+            if (fasterPointer.next == null){
+                break;
+                // (fasterPointer.next.next == null && fasterPointer.next != null) means the length of the list is even
+                // and at the point, slowerPointer locates at the last node of the middle point of the list
+                // so if I want it to be the next node of the middle point of the list, I need to 'next' it
+            } else if (fasterPointer.next.next == null) {
+                slowerPointer = slowerPointer.next;
+                break;
+            } else {
+                slowerPointer = slowerPointer.next;
+                fasterPointer = fasterPointer.next.next;
+            }
+        }
+        while (slowerPointer != null){
+            stack.add(slowerPointer.value);
+            slowerPointer = slowerPointer.next;
+        }
+        Node<V> firstPart = head;
+        while (firstPart.value.compareTo(stack.pop())==0){
+            if (stack.empty()){
+                return true;
+            }
+            firstPart = firstPart.next;
+        }
+        return false;
+    }
+
+    public static <V extends Comparable<V>> void sort(Node<V> head){
+        // I need to create 6 pointers (nodes) to trace the shared part
+        Node<V> smallHead = new Node<>(null);
+        Node<V> smallTail = new Node<>(null);
+        Node<V> equalHead = new Node<>(null);
+        Node<V> equalTail = new Node<>(null);
+        Node<V> largerHead = new Node<>(null);
+        Node<V> largerTail = new Node<>(null);
+    }
+
+
+
+
+
+
+
+
+
+
+
 
     public static void main(String[] args) {
         Node<Integer> head1 = new Node<>(1);
@@ -105,5 +199,25 @@ public class Code04 {
         printDoubleLinkedList(head2);
         printDoubleLinkedList(reverseList(head2));
 
+        Node<Integer> head3 = new Node<>(4);
+        head3.next = new Node<>(5);
+        head3.next.next = new Node<>(6);
+        head3.next.next.next = new Node<>(7);
+
+        printLinkedList(head1);
+        head1 = reverseList(head1);
+        printSharedPart(head1, head3);
+        printLinkedList(head1);
+
+
+        Node<Integer> head4 = new Node<>(4);
+        head4.next = new Node<>(5);
+        head4.next.next = new Node<>(6);
+        head4.next.next.next = new Node<>(6);
+        head4.next.next.next.next = new Node<>(5);
+        head4.next.next.next.next.next = new Node<>(4);
+
+        System.out.println(ifPalindrome(head1));
+        System.out.println(ifPalindrome(head4));
     }
 }
--- notes/04-LinkedList.md
+++ notes/04-LinkedList.md
@@ -4,4 +4,20 @@
 The inked list is a linear data structure storing elements, in which each is actually a separate object 
 while all the objects are **linked together by the reference field** in each element.
 
-### Singly-Linked List
+## Question
+### 1 - Reverse a SlinkedList
+
+### 2 - Reverse a DlinkedList
+
+### 3 - Print the shared part of two ordered SlinkedLists
+
+### 4 - Whether a linked list is Palindrome
+
+### 5 - Sort a SlinkedList
+Given the `head` node of a SlinkedList, with node value - Integer, and an integer `pivot`. Sort the list so that 
+the left part of the list is less than `pivot`, the middle part of the list is equal to the `pivot`, and the right part
+of the list is greater than the `pivot`.
+
+### 6 - SlinkedList Intersection
+
+## [Implementation](../code/Code04.java)
